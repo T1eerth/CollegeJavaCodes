879796a38582ea7c73438d4c9c23eb23bea51de4
Tirth Vanparia <tirth.vanparia16@gmail.com>
2022-08-04 23:37:50 +0530

    Add files via upload

---
+++ Range.java
@@ -0,0 +1,26 @@
+
+package com.mycompany.first;
+
+/**
+ *
+ * @author Tirth
+ */
+import java.util.Scanner;
+
+public class Range{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("");
+        System.out.print("Enter any number between 1 to 100 = ");
+        
+        int n = sc.nextInt();
+        
+        if(n>1 && n <100){            
+            System.out.println("Thank you.....Welcome again");
+        }
+        else{
+            System.out.println("Sorry, "+n+" is not between 1 to 100 ");
+        }
+    }
+}
+
