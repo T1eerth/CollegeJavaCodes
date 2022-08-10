c5b8b6fed6347846b8d592ae51f4eb401481d0d8
Tirth Vanparia <tirth.vanparia16@gmail.com>
2022-08-10 22:05:20 +0530

    Add files via upload

---
+++ Star Patterns/Inverted_half_pyramid.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.first;
+
+/**
+ *
+ * @author Tirth
+ */
+public class Inverted_half_pyramid {
+    public static void main(String[] args){
+        int n=4;
+        for(int i=n;i>=1;i--){
+            for(int j=i;j>=1;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
---
+++ Star Patterns/Rectangle_Stars.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.first;
+import java.util.*;
+
+
+public class Rectangle_Stars{
+    public static void main(String[] args) {
+        int m=4;       
+        int n=5;
+        for (int i=0;i<=m;i++){
+            for (int j=0;j<=n;j++){
+                System.out.print("*");
+            
+        }
+        System.out.println();
+        
+    }
+}
+
+}
