370acf8b6f48eba6a28c75d2618c80c31ca3a80d
Tirth Vanparia <tirth.vanparia16@gmail.com>
2022-08-10 23:41:04 +0530

    Add files via upload

---
+++ Star Patterns/Half_pyramid_numbers.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.first;
+
+/**
+ *
+ * @author Tirth
+ */
+public class Half_pyramid_numbers {
+    public static void main(String[] args){
+        int n=4;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+       
+    }
+    
+}
---
+++ Star Patterns/Inverted_half_pyramid_numbers.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.first;
+
+/**
+ *
+ * @author Tirth
+ */
+public class Inverted_half_pyramid_numbers {
+    public static void main(String[] args){
+        int n=4;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+       
+    }
+    
+}
